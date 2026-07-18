140a035997faac51571d92439f882eba7b15ed42
alfannurfaiz9 <alfannurfaizwork@gmail.com>
2026-07-18 23:21:52 +0700

    2

--- 2.js
+++ 2.js
@@ -1,10 +1,9 @@
 const multiplicationTable = (num) => {
-  let n = 1;
   let baris = "";
 
   for (let i = 1; i <= num; i++) {
     for (let j = 1; j <= num; j++) {
-      baris += " " + n++;
+      baris += " " + i * j;
     }
 
     console.log(baris);
